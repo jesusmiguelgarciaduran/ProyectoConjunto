194aa296e0c38c6c11221b246f60f96851f533d0
jesusmiguelgarciaduran <jesusmiguelgarciaduran@gmail.com>
2021-10-20 14:24:15 +0200

    segundo proyecto con fallos

--- ProyectoEnConjunto/src/main/java/com/example/ProyectoEnConjunto/ProyectoEnConjuntoApplication.java
+++ ProyectoEnConjunto/src/main/java/com/example/ProyectoEnConjunto/ProyectoEnConjuntoApplication.java
@@ -1,13 +1,13 @@
 package com.example.ProyectoEnConjunto;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class ProyectoEnConjuntoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectoEnConjuntoApplication.class, args);
-	}
-
-}
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class ProyectoEnConjuntoApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ProyectoEnConjuntoApplication.class, args);
+//	}
+//
+//}
---
+++ ProyectoSpringBoot/src/main/java/com/example/ProyectoSpringBoot/SaludoProperties.java
@@ -0,0 +1,16 @@
+package com.example.ProyectoSpringBoot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("saludo.mensaje")
+public class SaludoProperties {
+	private String idioma = "Castellano";
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+}
