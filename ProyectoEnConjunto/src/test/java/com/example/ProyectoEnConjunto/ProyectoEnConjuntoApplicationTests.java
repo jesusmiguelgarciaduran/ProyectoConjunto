662ec454ee53d1171d8ccb3d9dd3a2d44e42f701
jesusmiguelgarciaduran <jesusmiguelgarciaduran@gmail.com>
2021-10-20 08:51:35 +0200

    Primer commit

---
+++ ProyectoEnConjunto/src/test/java/com/example/ProyectoEnConjunto/ProyectoEnConjuntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoEnConjunto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEnConjuntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
